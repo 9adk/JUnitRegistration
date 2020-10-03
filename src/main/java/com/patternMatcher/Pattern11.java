c9e12da4656439358aa4239cdb3860e77e1ee27d
aditya Kharade <adityakharade51@gmail.com>
2020-10-03 07:44:33 +0530

    Added : added functionality to validate Emails

--- src/main/java/com/patternMatcher/Pattern11.java
+++ src/main/java/com/patternMatcher/Pattern11.java
@@ -4,15 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Pattern11 {
-	public String validateInput(String name) {
-		String expression = "(^[A-Z]{1})[a-z]{2,}$"; // Pattern for Names
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(name);
-		if(match.find()) {
-			return "valid";
-		}
-		return "invalid";
-	}
+	
 	public String validateEmail(String Email) {
 		String expression = "(abc?[.][A-Za-z]*@bl[.]co[.][A-Za-z]{2,})$"; // Pattern for Email
 		Pattern pattern = Pattern.compile(expression);
@@ -22,25 +14,6 @@ public class Pattern11 {
 		}
 		return "invalid";
 	}
-	public String validateNumber(String number) {
-		String expression = "^[0-9]{2}?[ ][0-9]{4,13}";	// Pattern for Number
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(number);
-		if(match.find()) {
-			return "valid";
-		}
-		return "invalid";
-		
-	}
-	public String validatePassword(String pass) {
-		String expression = "(?=.*[A-Z])(?=.*\\d)((?=.*[@$!%*?&]){1})[A-Za-z0-9]{8,}";	//Pattern for Password
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(pass);
-		if(match.find()) {
-			return "valid";
-		}
-		return "invalid";
-		
-	}
+	
 	
 }
--- src/test/java/com/patternMatcher/PatternTest.java
+++ src/test/java/com/patternMatcher/PatternTest.java
@@ -2,36 +2,39 @@ package com.patternMatcher;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.*;
 
-class PatternTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class PatternTest {
 	
-	@Test
-	public void givenEntry_whenAllDataValid_thenReturnHappyMood() {
-		
-		Pattern11 obj = new Pattern11();
-		String name = obj.validateInput("Aditya");
-		String email = obj.validateEmail("abc.xyz@bl.co.in");
-		String number = obj.validateNumber("917887483853");
-		String password = obj.validatePassword("Qwerty@123");		
-		assertTrue(name == "valid" && email == "valid" && number == "valid" && password == "valid");
-		System.out.println("Happy Mood");
-		
-			
+	private String email;
+	private String expectedValue;
+	public PatternTest(String email, String expectedValue) {
+		super();
+		this.email = email;
+		this.expectedValue = expectedValue;
 	}
 	
-	@Test
-	public void givenEntry_whenAllDataInalid_thenReturnSadMood() {
-		
+	@Before
+	public void initialize() {
 		Pattern11 obj = new Pattern11();
-		String name = obj.validateInput("Aditya");
-		String email = obj.validateEmail("abc.xyz@bl.co.in");
-		String number = obj.validateNumber("917887483853");
-		String password = obj.validatePassword("Qwerty@123");			
-		assertTrue(name == "invalid" || email == "invalid" || number == "invalid" || password == "invalid");
-		System.out.println("Sad Mood");
-		
-			
 	}
-
+	@Parameterized.Parameters
+	public static Collection input() {
+		return Arrays.asList(new Object[][] {{"abc-100@yahoo.com", "valid"}, {"abc@yahoo.com", "valid"}, {"abc.100@yahoo.com", "valid"}, 
+		{"abc111@abc.com", "valid"}, {"abc-100@abc.net", "valid"}, {"abc.100@abc.com.au", "valid"}, {"abc@1.com", "valid"}, {"abc@gmail.com.com", "valid"}, 
+		{"abc+100@gmail.com", "valid"}});
+	}	
+	@Test
+	public void givenSamples_whenExpectedIsTrue_returnTrue() {
+		Pattern11 obj1 = new Pattern11();
+		
+	    assertEquals(expectedValue, obj1.validateEmail(email));
+	        
+    }
 }
